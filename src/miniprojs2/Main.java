50e115d04762f1a1ba6d323ab7c70546c66fb93c
bretx <bretx@PC-A102-09.iut-valence.fr>
2016-02-19 16:12:00 +0100

    New class main
    creating the main class

---
+++ src/miniprojs2/Main.java
@@ -0,0 +1,16 @@
+package miniprojs2;
+
+public class Main
+{
+
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		game g1 = new game();
+		g1.play();
+		g1.getResultat();
+		
+	}
+
+}
